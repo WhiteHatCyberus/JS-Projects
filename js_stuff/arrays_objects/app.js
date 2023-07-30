f4546e73335869a8700cefe1edc21cc36a67cb00
Sleety Matt George <70995581+WhiteHatCyberus@users.noreply.github.com>
2023-07-30 16:49:08 +0530

    practiced arrays and objects

---
+++ js_stuff/arrays_objects/app.js
@@ -0,0 +1,15 @@
+var clothes=[];
+clothes.push("tshirt");
+clothes.push("cap");
+clothes.push("crop");
+clothes.push("trainer");
+clothes.push("activewear");
+console.log(clothes);
+clothes.pop()
+console.log(clothes);
+clothes.push("activewear");
+console.log(clothes);
+
+var favCar={};
+favCar.convertible="True";
+console.log(favCar)
